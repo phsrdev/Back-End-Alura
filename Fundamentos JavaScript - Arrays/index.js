f82b3adc099f7870285b9a9c2371daf4ad34b2f6
Pedro Rodrigues <101387000+phsrdev@users.noreply.github.com>
2022-05-24 17:17:31 -0300

    2° Curso de Fundamentos de JS
    Aprendendo sobre arrays, aula 1 - aprendendo a criar, incluir e deletar itens de arrays em JS

---
+++ Fundamentos JavaScript - Arrays/index.js
@@ -0,0 +1,16 @@
+const arrayVazia = [];
+
+console.log(arrayVazia)
+console.log(arrayVazia.length)
+console.log(arrayVazia[0])
+
+const arrayVazia2 = [,,,];
+console.log(arrayVazia2.length)
+console.log(arrayVazia2[0])
+console.log(arrayVazia2[1])
+console.log(arrayVazia2[2])
+console.log(arrayVazia.length)
+
+arrayVazia.push(50)
+console.log(arrayVazia)
+console.log(arrayVazia.length)
