1cdde90ba648d5a8cc6d3137d6d182a93fc5553e
Pedro Rodrigues <101387000+phsrdev@users.noreply.github.com>
2022-05-29 23:13:59 -0300

    Finalizando o Curso
    Nessa última aula, aprendi:
    Que chamamos de polimorfismo a alteração de um método de uma classe para que, na subclasse, o método tenha a mesma assinatura porém um comportamento diferente do método executado no contexto da superclasse;
    Que existe uma série de princípios de design de código pensados para a programação orientada a objetos, que estão compilados no acrônimo SOLID.

--- JavaScript programacao orientada a objetos/index.js
+++ JavaScript programacao orientada a objetos/index.js
@@ -2,10 +2,13 @@ import User from "./User.js";
 import Docente from "./Docente.js";
 import Admin from "./Admin.js";
 
-// const novoUser = new User('Mariana', 'm@m.com', '2021-01-01')
+// const novoUser = new User('Juliana', 'j@j.com', '2021-01-01')
 // console.log(novoUser.exibirInfos())
 
-const novoAdmin = new Admin('Pedro', 'p@p.com', '1992-01-01')
-console.log(novoAdmin.nome)
-novoAdmin.nome = ''
-console.log(novoAdmin.nome)
+// const novoAdmin = new Admin('Rodrigo', 'r@r.com', '2021-01-01')
+// console.log(novoAdmin.nome)
+// novoAdmin.nome = ''
+// console.log(novoAdmin.nome)
+
+const novoDocente = new Docente('Pedro', 'p@p.com', '2021-01-01')
+console.log(novoDocente.exibirInfos())
